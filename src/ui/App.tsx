ec887fd61eda4a310e2795f740914b2afba310b5
ranger00pr0 <wilmersolano777@gmail.com>
2025-12-12 16:11:48 -0500

    fix(electron): use HashRouter and relative paths to fix white screen

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -25,11 +25,11 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <AuthWrapper />
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 };
